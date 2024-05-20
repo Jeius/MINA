2a227e706ce226639f8fac0e9a7fa58a0652bee9
Julius Pahama <pahamajulius@gmail.com>
2024-05-20 15:15:41 +0800

    merged location-provider and location-services

--- src/components/context-providers/location-provider.tsx
+++ src/components/context-providers/location-provider.tsx
@@ -1,22 +1,74 @@
 'use client';
 
-import { PropsWithChildren, createContext, useContext } from 'react';
-import { LocationComponent, location } from '@/models/location-services';
+import { LatLng } from 'leaflet';
+import { useState, useEffect, PropsWithChildren, createContext, useContext } from 'react';
+
+
+type GeolocationPosition = {
+    coords: {
+        latitude: number;
+        longitude: number;
+    };
+};
+
+type GeolocationError = {
+    message: string;
+};
+
+type Location = {
+    location: LatLng | null,
+    error: string | null,
+};
+
+const [location, setLocation] = useState<Location | null>(null);
 
 const Context = createContext(location);
 
-export const AppContext: React.FC<PropsWithChildren> = ({ children }) => {
+const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000,
+};
+
+const onSuccess = (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+    setLocation({
+        location: new LatLng(latitude, longitude),
+        error: null,
+    });
+};
+
+const onError = (error: GeolocationError) => {
+    setLocation({
+        location: null,
+        error: `Error: ${error.message}`,
+    });
+};
+
+const LocationComponent: React.FC<PropsWithChildren> = ({ children }) => {
+    useEffect(() => {
+        if (!navigator.geolocation) {
+            onError({
+                message: 'Geolocation is not supported by your browser'
+            });
+            return;
+        }
+
+        navigator.geolocation.watchPosition(
+            onSuccess, onError, options,
+        );
+    }, []);
 
     return (
-        <LocationComponent>
-            <Context.Provider value={location}>
-                {children}
-            </Context.Provider>
-        </LocationComponent>
-
+        <Context.Provider value={location}>
+            {children}
+        </Context.Provider>
     );
 }
 
-export const useAppContext = () => {
+const useLocationContext = () => {
     return useContext(Context);
 }
+
+
+export { LocationComponent, location, useLocationContext };
