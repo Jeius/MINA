8e0633111bfe97b7266eac2ced45e33d78e908fe
Julius Pahama <pahamajulius@gmail.com>
2024-05-31 16:22:38 +0800

    feature(SearchField):Create a search field component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { LocationProvider } from '@/context-providers/location-provider';
 import dynamic from 'next/dynamic';
+import SearchField from '@/components/ui/search-field';
 
 const CampusMap = dynamic(() => import('@/components/ui/campus-map'), { ssr: false })
 
 const Home: React.FC = () => {
     return (
         <main className='flex flex-col h-screen w-screen bg-gray-200'>
-            <LocationProvider> <CampusMap /> </LocationProvider>
+            <LocationProvider> <CampusMap /> <SearchField /> </LocationProvider>
         </main>
     );
 }
---
+++ src/components/ui/search-field.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchField: React.FC = async () => {
+    
+    return (
+        <form className={`fixed flex top-0 place-self-center w-10/12 h-auto m-4 py-2 px-4
+        backdrop-blur-md bg-black bg-opacity-70 rounded-2xl outline outline-1 outline-slate-100`} >
+           Search Field
+        </form>
+    )
+}
+
+export default SearchField;
