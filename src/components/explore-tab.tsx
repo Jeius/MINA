54eaaf0f7dce06c1135ec5dc692f71b570b0b814
Julius Pahama <pahamajulius@gmail.com>
2024-06-05 22:53:11 +0800

    change(SearchField): Change the tag of search field from form to input.

--- src/components/explore-tab.tsx
+++ src/components/explore-tab.tsx
@@ -8,7 +8,7 @@ const ExploreTab = async () => {
     // console.log(facilities);
     return (
         <section className='absolute flex justify-center top-0 left-0 right-0'>
-            <SearchField className='relative m-4 py-2 px-4' />
+            <SearchField className='relative m-4 py-2 px-4' placeholder="Search for places" />
         </section>
     )
 }
--- src/components/ui/search-field.tsx
+++ src/components/ui/search-field.tsx
@@ -1,16 +1,20 @@
 import { cn } from "@/lib/utils";
 import { HTMLAttributes } from "react";
 
-type Props = HTMLAttributes<HTMLElement>;
+type Props = HTMLAttributes<HTMLElement> & {
+    placeholder?: string,
+};
 
-const SearchField: React.FC<Props> = ({ className }) => {
+const SearchField: React.FC<Props> = ({ className, placeholder }) => {
     const glassStyle = 'backdrop-blur-md bg-black bg-opacity-70';
-    const outline = 'rounded-2xl outline outline-1 outline-slate-100';
+    const outline = 'rounded-2xl outline outline-1 outline-slate-100 focus-visible:outline-primary';
     const size = 'w-10/12 h-auto max-w-xl';
+    const textStyle = 'px-3 py-2 text-sm text-white';
+    const placeholderStyle = 'placeholder:text-slate-300';
     return (
-        <form className={cn(glassStyle, outline, size, className)}>
-            Search here
-        </form>
+        <input placeholder={placeholder} className={
+            cn(`${glassStyle} ${outline} ${size} ${textStyle} ${placeholderStyle}`, className)
+        } />
     )
 }
 
