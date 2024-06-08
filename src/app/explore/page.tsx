d1041a5eaa4ec7f73c8a972f4f6c1d6bedb8f003
Julius Pahama <pahamajulius@gmail.com>
2024-06-09 03:39:10 +0800

    change(tabs): Change the routing of tabs.

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,5 +1,5 @@
 import { getFacilities } from "@/api/prisma-Fetchers"
-import SearchField from "./ui/search-field";
+import SearchField from "@/components/ui/search-field";
 
 
 
@@ -7,11 +7,11 @@ const ExploreTab = async () => {
     // const facilities = await getFacilities();
     // console.log(facilities);
     return (
-        <>
-            <form role="search" className='absolute flex justify-center top-0 left-0 right-0'>
+        <section className="z-50">
+            <form role="search" className='absolute z-50 flex justify-center top-0 left-0 right-0'>
                 <SearchField className='relative m-4 py-2 px-4' placeholder="Search for places" />
             </form>
-        </>
+        </section>
     )
 }
 
