34aff802874cdb0c907133837990ece8bddd956e
Julius Pahama <pahamajulius@gmail.com>
2024-06-09 03:52:12 +0800

    route(animatedButton): Create a function to push route when button is clicked

--- src/components/ui/animated-button.tsx
+++ src/components/ui/animated-button.tsx
@@ -3,8 +3,8 @@
 import { useAppContext } from '@/lib/context';
 import { cn } from '@/lib/utils';
 import { AnimatePresence, motion } from 'framer-motion';
-import Link from 'next/link';
-import React, { HTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
+import { useRouter } from 'next/navigation';
 
 type Props = HTMLAttributes<HTMLElement> & {
     name: string,
@@ -13,6 +13,8 @@ type Props = HTMLAttributes<HTMLElement> & {
 
 const AnimatedButton: React.FC<Props> = ({ className, name, icon, }) => {
     const { selectedTab, setSelectedTab } = useAppContext();
+    const router = useRouter();
+
     const bg = selectedTab == name ? 'bg-primary' : 'bg-transparent';
     const style = 'relative flex items-center w-auto z-10 sm:mx-0 md:mx-1 lg:mx-3 px-5 py-2 rounded-full';
     const textStyle = 'text-sm font-medium overflow-clip';
@@ -21,6 +23,9 @@ const AnimatedButton: React.FC<Props> = ({ className, name, icon, }) => {
         if (setSelectedTab) {
             setSelectedTab(newTab);
         }
+
+        router.push(`/${newTab.toLowerCase()}`);
+
     };
 
     return (
