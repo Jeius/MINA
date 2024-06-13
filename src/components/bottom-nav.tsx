983860a196ff8f6f25c574aa6d9979d8019f0557
Julius Pahama <pahamajulius@gmail.com>
2024-06-13 21:46:05 +0800

    change(bottomNav): Change the bottom nav into server component

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -1,13 +1,10 @@
-"use client"
-
 import { ExploreSVG, DirectionsSVG, PlacesSVG } from './ui/icons';
 import { AnimatedButton } from "@/components/ui/animated";
-import { usePathname } from 'next/navigation';
 
 const glassStyling = 'backdrop-blur-md bg-black bg-opacity-80';
 const position = 'fixed bottom-0';
 const style = 'py-3 w-full flex justify-center';
-const iconStyle = 'fill-white drop-shadow-lg size-5 mr-2'
+const iconStyle = 'fill-white drop-shadow-lg size-5 mr-2';
 
 const tabs: { [key: string]: React.ReactNode } = {
     Explore: <ExploreSVG className={iconStyle} />,
@@ -16,16 +13,12 @@ const tabs: { [key: string]: React.ReactNode } = {
 };
 
 const BottomNav = () => {
-    const pathname = usePathname();
-    const activeTab = pathname.substring(1) || 'explore';
-
     return (
         <nav className={`${position} ${style} ${glassStyling}`}>
             {Object.entries(tabs).map(([tabName, icon]) => (
                 <AnimatedButton
                     key={tabName}
                     name={tabName}
-                    active={activeTab}
                     icon={icon}
                 />
             ))}
