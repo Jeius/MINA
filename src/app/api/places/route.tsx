e908cb59c08ce4657d76b4910a6721179526c317
Julius Pahama <pahamajulius@gmail.com>
2024-06-14 13:08:29 +0800

    create(getPlaces): Create an api to fetch places coordinates.

---
+++ src/app/api/places/route.tsx
@@ -0,0 +1,44 @@
+
+import { db } from '@/lib/utils';
+import { NextResponse } from 'next/server';
+
+const getPlaces = async () => {
+    try {
+        const facilities = await db.facility.findMany({
+            select: {
+                id: true,
+                name: true,
+                node: {
+                    select: {
+                        x_coord: true,
+                        y_coord: true,
+                    }
+                }
+            },
+        });
+
+        const rooms = await db.room.findMany({
+            select: {
+                id: true,
+                building_id: true,
+                name: true,
+                x_coord: true,
+                y_coord: true,
+            },
+        });
+
+        return { facilities: facilities, rooms: rooms };
+    } catch (error: any) {
+        console.error(error.message);
+        throw error;
+    }
+}
+
+export const GET = async () => {
+    try {
+        const results = await getPlaces();
+        return NextResponse.json(results);
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to fetch place coordinates' }, { status: 500 });
+    }
+}
