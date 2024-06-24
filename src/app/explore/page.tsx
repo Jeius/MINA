abab40726334ad3e81a18e169999e93ab9e4d2a7
Julius Pahama <pahamajulius@gmail.com>
2024-06-24 23:24:07 +0800

    add(sections): Add roles to sections in layout and explore page.

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -20,7 +20,7 @@ const ExploreTab = async ({ searchParams }: ExploreTabProps) => {
     const size = 'w-full max-h-80';
 
     return (
-        <section className="w-full max-w-xl place-self-center flex flex-col p-3 z-10">
+        <section role='search' className="w-full max-w-xl place-self-center flex flex-col p-3 z-10">
             <SearchField placeholder="Search for places" />
             {(query && show) &&
                 <AnimatedUl className={cn(scroll, style, outline, size)}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               <strong>MSU-IIT Campus Navigation Guide</strong>
             </header>
 
-            <section className='flex justify-around pointer-events-auto'>
+            <section role='main' className='relative flex justify-around pointer-events-auto'>
               {children}
             </section>
 
