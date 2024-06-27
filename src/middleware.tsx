82a8b1f367498588c5e95c629bd998dc37f321f1
Julius Pahama <pahamajulius@gmail.com>
2024-06-28 02:06:58 +0800

    change(middleware): Add default center and zoom in hash

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -5,6 +5,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
+    const hash = '#map=16/8.241531/124.243855'
     const defaultPath = '/explore';
 
     // Check if the user is at the root URL without any hash
@@ -12,6 +13,7 @@ export function middleware(request: NextRequest) {
         // Redirect to the map page with default parameters
         const url = request.nextUrl.clone();
         url.pathname = defaultPath;
+        url.hash = hash;
         return NextResponse.redirect(url);
     }
 
