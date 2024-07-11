57bd9908dac6eb6bc8f3377054d025647b94e496
Julius Pahama <pahamajulius@gmail.com>
2024-07-11 16:28:58 +0800

    change(SearchBox): Move the independent hooks to SearchBox from Search.

--- src/app/explore/search.tsx
+++ src/app/explore/search.tsx
@@ -1,66 +1,19 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import debounce from "debounce";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 import SearchBox from "../../components/SearchBox";
 
 
 const Search = () => {
     const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const router = useRouter();
-
-    const [focus, setFocus] = useState(false);
     const [search, setSearch] = useState(new URLSearchParams(searchParams).get('query'));
-    const debouncedsetSearch = debounce(setSearch, 500);
-
-
-    const handleClear = useCallback(() => {
-        setSearch('');
-    }, []);
-
-    const handleFocus = useCallback((e: React.FocusEvent<HTMLInputElement, Element>) => {
-        e.preventDefault();
-        setFocus(true);
-    }, []);
-
-    const handleBlur = useCallback((e: React.FocusEvent<HTMLInputElement, Element>) => {
-        e.preventDefault();
-        setFocus(false);
-    }, []);
-
-    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        debouncedsetSearch(value);
-    }, [debouncedsetSearch]);
-
-    useEffect(() => {
-        const params = new URLSearchParams(searchParams);
-        const hash = window.location.hash;
-        if (search && focus) {
-            params.set('query', search);
-        } else {
-            params.delete('query');
-        }
-        if (focus) {
-            params.set('s', focus.toString());
-        } else {
-            params.delete('s');
-        }
-        router.replace(`${pathname}?${params.toString()}${hash}`, { scroll: false });
-    }, [focus, pathname, router, search, searchParams]);
-
 
     return (
         <SearchBox
             placeholder='Search for places'
-            defaultValue={search || ''}
-            focus={focus}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            onClear={handleClear}
+            query={search}
+            setQuery={setSearch}
         />
     )
 }
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,20 +1,30 @@
 "use client"
 import { cn } from "@/lib/utils";
-import { memo, useRef } from "react";
+import { Dispatch, memo, SetStateAction, useCallback, useEffect, useRef, useState } from "react";
 import { CancelSVG, SearchSVG } from "./ui/icons";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import debounce from "debounce";
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
-    focus?: boolean,
+    query: string | null,
+    setQuery: Dispatch<SetStateAction<string | null>>,
     onClear?: () => void,
 };
 
 
 const SearchBox: React.FC<Props> = ({
     className,
-    focus,
+    query,
+    setQuery,
     onClear,
     ...props
 }) => {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const router = useRouter();
+    const [focus, setFocus] = useState(false);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
     const glass = 'backdrop-blur-md bg-black bg-opacity-70';
     const outline = `rounded-xl outline outline-1 `;
     const outlineColor = focus ? 'outline-slate-200' : 'outline-slate-400';
@@ -23,7 +33,21 @@ const SearchBox: React.FC<Props> = ({
     const style = 'relative flex items-center justify-center px-4 py-2';
     const iconStyle = 'h-full w-auto relative flex items-center fill-white';
 
-    const inputRef = useRef<HTMLInputElement | null>(null);
+    const debouncedsetSearch = debounce(setQuery, 500);
+
+    const handleClear = useCallback(() => {
+        if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+        setQuery('');
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        debouncedsetSearch(value);
+    }, [debouncedsetSearch]);
+
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -31,12 +55,31 @@ const SearchBox: React.FC<Props> = ({
         }
     };
 
-    const handleClear = () => {
-        if (inputRef.current) {
-            inputRef.current.value = '';
+    const handleFocus = useCallback((e: React.FocusEvent<HTMLInputElement, Element>) => {
+        e.preventDefault();
+        setFocus(true);
+    }, []);
+
+    const handleBlur = useCallback((e: React.FocusEvent<HTMLInputElement, Element>) => {
+        e.preventDefault();
+        setFocus(false);
+    }, []);
+
+    useEffect(() => {
+        const params = new URLSearchParams(searchParams);
+        const hash = window.location.hash;
+        if (query && focus) {
+            params.set('query', query);
+        } else {
+            params.delete('query');
         }
-        onClear && onClear();
-    };
+        if (focus) {
+            params.set('s', focus.toString());
+        } else {
+            params.delete('s');
+        }
+        router.replace(`${pathname}?${params.toString()}${hash}`, { scroll: false });
+    }, [focus, pathname, router, query, searchParams]);
 
 
     return (
@@ -52,11 +95,15 @@ const SearchBox: React.FC<Props> = ({
                 role='searchbox'
                 autoComplete='off'
                 ref={inputRef}
+                defaultValue={query || ''}
                 onKeyDown={handleKeyDown}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onChange={handleChange}
                 className='w-full h-auto mr-2 text-sm placeholder:text-slate-300 bg-transparent outline-none'
                 {...props}
             />
-            {inputRef.current?.value
+            {query
                 ? <CancelSVG role='button' onClick={handleClear} className={iconStyle} />
                 : <SearchSVG role='img' className={iconStyle} />}
 
