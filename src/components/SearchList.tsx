21ecffb5470244617381acbc530a2e3ca60a6fc9
Julius Pahama <pahamajulius@gmail.com>
2024-07-16 22:13:30 +0800

    revise(SearchList): Add click handler for the lists. Remove the show flag to render the lists when the SearchBox is in focus.

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,74 +1,91 @@
-
-import { Places } from "@/lib/model";
+"use client"
+import { Place, Places } from "@/lib/model";
 import { AnimatedLi, AnimatedUl, } from "./ui/animated";
-import { stringToBoolean } from "@/lib/utils";
+import { cn, stringToBoolean } from "@/lib/utils";
 import Link from "next/link";
 import { useFetchPlaces } from "@/lib/fetch-hooks";
 import { useEffect, useState } from "react";
 import { CustomSkeleton } from "./ui/skeleton";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { updateHash } from "./MapEventsHandler";
 
-const highlightText = (text: string, query: string) => {
-    if (!query) return text;
+const highlightText = (text: string, search: string) => {
+    if (!search) return text;
 
-    const parts = text.split(new RegExp(`(${query})`, 'gi'));
+    const parts = text.split(new RegExp(`(${search})`, 'gi'));
     return parts.map((part, index) =>
-        part.toLowerCase() === query.toLowerCase() ? <strong key={index}>{part}</strong> : part
+        part.toLowerCase() === search.toLowerCase() ? <strong key={index}>{part}</strong> : part
     );
 };
 
 type Props = {
-    pathName: string,
+    pathname: string,
 }
 
 const SearchList: React.FC<Props> = ({
-    pathName,
+    pathname,
 }) => {
     const searchParams = useSearchParams();
-    const query = searchParams.get('query');
-    const show = stringToBoolean(searchParams.get('s') || undefined);
+    const router = useRouter();
+    const search = searchParams.get('search');
 
     const { places, isError, isLoading } = useFetchPlaces();
     const [searchResult, setSearchResult] = useState<Places>();
 
-    useEffect(() => {
-        const q = Array.isArray(query) ? query.join(',') : query; // Handle array case
-        if (!q) return;
-        const filteredPlaces = places?.filter(place => place.name.toLowerCase().includes(q.toLowerCase()));
-        setSearchResult(filteredPlaces);
-    }, [query, places]);
+    const handleClick = (result: Place, e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        const name = result.facility ? `${result.name}, ${result.facility}` : result.name;
+        const zoom = 20;
+        const lat = result.position[0];
+        const lng = result.position[1];
+        const hash = `#map=${zoom}/${lat}/${lng}`;
+        const params = new URLSearchParams(searchParams);
+        params.set('name', name);
 
-    return ((query && show) &&
-        <AnimatedUl className='relative flex flex-col rounded-xl my-2 px-3 py-2 w-full max-h-80 outline outline-1 outline-slate-500 overflow-x-auto overscroll-none scrollbar-thin'>
+        e.preventDefault();
+
+        router.replace(`${pathname}?${params.toString()}`);
+        window.location.hash = hash;
+    }
+
+    useEffect(() => {
+        const query = Array.isArray(search) ? search.join(',') : search; // Handle array case
+        if (!query) return;
+
+        const filteredPlaces = places?.filter(place => place.name.toLowerCase().includes(query.toLowerCase()));
+        setSearchResult(filteredPlaces);
+    }, [search, places]);
+
+    return (search &&
+        <AnimatedUl className={cn(
+            'relative flex flex-col rounded-xl my-2 px-3 py-2 w-full max-h-80 outline outline-1',
+            'outline-slate-500 overflow-x-auto overscroll-none scrollbar-thin',
+        )}>
             {isLoading && <CustomSkeleton />}
+
             {isError &&
                 <AnimatedLi
                     key="noResults"
                     className="place-self-center text-sm overflow-hidden">
-                    Failed to search {query}
-                </AnimatedLi>
-            }
+                    Failed to search {search}
+                </AnimatedLi>}
+
             {searchResult?.length !== 0
-                ? searchResult?.map((result, index) => (
-                    <AnimatedLi
+                ? searchResult?.map((result, index) => {
+                    const name = result.facility ? `${result.name}, ${result.facility}` : result.name;
+                    return <AnimatedLi
                         key={index}
-                        className='min-h-14 my-1 outline outline-1 outline-primary-dark bg-primary rounded-xl bg-opacity-90'
+                        onClick={e => handleClick(result, e)}
+                        className={cn(
+                            'min-h-14 my-1 outline outline-1 outline-primary-dark',
+                            'size-full p-2 bg-primary rounded-xl bg-opacity-90',
+                            'relative flex items-center text-sm cursor-pointer'
+                        )}
                     >
-                        <Link
-                            href={`/${pathName}?name=${result.name}#map=19/${result.position[0]}/${result.position[1]}`}
-                            role='button'
-                            scroll={false}
-                            className="size-full p-2 relative flex items-center text-sm cursor-pointer"
-                        >
-                            <span>
-                                {result.facility
-                                    ? highlightText(`${result.name}, ${result.facility}`, typeof query === 'string' ? query : query![0])
-                                    : highlightText(result.name, typeof query === 'string' ? query : query![0])
-                                }
-                            </span>
-                        </Link>
+                        <span>
+                            {highlightText(name, typeof search === 'string' ? search : search![0])}
+                        </span>
                     </AnimatedLi>
-                ))
+                })
                 : <AnimatedLi
                     key="noResults"
                     className="place-self-center text-sm overflow-hidden">
