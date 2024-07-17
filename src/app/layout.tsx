d99e183b7ff01daf8c0cadb0694ff42ea5ec3415
Julius Pahama <pahamajulius@gmail.com>
2024-07-17 19:43:05 +0800

    Revise(layout): Revise the root layout. Add viewport and fixed the overflow of bottom-nav.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import AppContext from "@/lib/context";
 import dynamic from "next/dynamic";
 import { cn } from "@/lib/utils";
+import { Viewport } from "next";
 
 const MapComponent = dynamic(() => import('@/components/map-component'), { ssr: false });
 const BottomNav = dynamic(() => import('@/components/bottom-nav'), { ssr: false });
@@ -16,20 +17,27 @@ export const metadata = {
   description: "MSU-IIT Navigation Aid",
 };
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  height: 'device-height',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const glass = 'backdrop-blur-md bg-black bg-opacity-80';
   const outline = `outline outline-1 outline-slate-500`;
-  const textStyle = 'text-white text-md';
+  const textStyle = 'text-white text-sm';
   const style = 'p-3 justify-center pointer-events-auto';
 
   return (
     <AppContext>
-      <html lang="en" className={`${inter.className} ${scrollbar} text-white text-sm overflow-hidden`}>
-        <body className='relative h-screen w-screen bg-gray-300'>
+      <html lang="en">
+        <body className={`fixed top-0 bottom-0 left-0 right-0 bg-gray-300 ${inter.className} ${scrollbar} text-white`}>
           <MapComponent />
 
-          <main className='relative size-full flex flex-col justify-items-center z-20 pointer-events-none'>
+          <main className='relative size-full flex-wrap flex flex-col justify-items-center z-20 pointer-events-none'>
             <header className={cn(style, glass, outline, textStyle)}>
               <strong>MSU-IIT Campus Navigation Guide</strong>
             </header>
@@ -38,7 +46,7 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               {children}
             </section>
 
-            <footer className='w-full pointer-events-auto'>
+            <footer className='relative w-full pointer-events-auto'>
               <BottomNav />
             </footer>
           </main>
