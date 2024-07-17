a4d92ca83aafab886c09befdd64039ee07ee930e
Julius Pahama <pahamajulius@gmail.com>
2024-07-17 11:22:35 +0800

    add(animated): Add AnimatedDiv.

--- src/components/ui/animated.tsx
+++ src/components/ui/animated.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from '@/lib/utils'
-import { MotionProps, motion } from 'framer-motion'
+import { AnimatePresence, MotionProps, motion } from 'framer-motion'
 import React from 'react'
 
 type AnimatedProps = React.HTMLAttributes<HTMLElement> & MotionProps;
@@ -10,14 +10,17 @@ export const AnimatedSpan: React.FC<AnimatedProps> = ({
     children,
     ...props }) => {
     return (
-        <motion.span
-            initial={{ opacity: 0, width: 0, }}
-            animate={{ width: 'auto', opacity: 1, }}
-            exit={{ opacity: 0, width: 0, }}
-            {...props}
-        >
-            {children}
-        </motion.span>
+        <AnimatePresence>
+            <motion.span
+                initial={{ opacity: 0, width: 0, }}
+                animate={{ width: 'auto', opacity: 1, }}
+                exit={{ opacity: 0, width: 0, }}
+                {...props}
+            >
+                {children}
+            </motion.span>
+        </AnimatePresence>
+
     )
 }
 
@@ -27,14 +30,17 @@ export const AnimatedUl: React.FC<AnimatedProps> = ({
     ...props }) => {
     const glass = "drop-shadow-lg backdrop-blur-md bg-black bg-opacity-70";
     return (
-        <motion.ul
-            initial={{ opacity: 0, height: 0, }}
-            animate={{ height: 'auto', opacity: 1, }}
-            className={cn(glass, className)}
-            {...props}
-        >
-            {children}
-        </motion.ul>
+        <AnimatePresence>
+            <motion.ul
+                initial={{ opacity: 0, height: 0, }}
+                animate={{ height: 'auto', opacity: 1, }}
+                exit={{ opacity: 0, height: 0, }}
+                className={cn(glass, className)}
+                {...props}
+            >
+                {children}
+            </motion.ul>
+        </AnimatePresence>
     )
 };
 
@@ -51,4 +57,24 @@ export const AnimatedLi: React.FC<AnimatedProps> = ({
             {children}
         </motion.li>
     );
+};
+
+export const AnimatedDiv: React.FC<AnimatedProps> = ({
+    className,
+    children,
+    ...props }) => {
+    const glass = "drop-shadow-lg backdrop-blur-md bg-black bg-opacity-70";
+    return (
+        <AnimatePresence>
+            <motion.div
+                initial={{ opacity: 0, height: 0, }}
+                animate={{ height: 'auto', opacity: 1, }}
+                exit={{ opacity: 0, height: 0, }}
+                className={cn(glass, className)}
+                {...props}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    )
 };
