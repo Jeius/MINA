8663caded8fa7be501a6c0103d0c239e5bcdcd66
Julius Pahama <pahamajulius@gmail.com>
2024-07-17 23:05:27 +0800

    create(InfoSheet): Create click handlers.

--- src/components/info-sheet.tsx
+++ src/components/info-sheet.tsx
@@ -1,16 +1,18 @@
 "use client"
-import { cn } from "@/lib/utils"
+import { cn, updateHash } from "@/lib/utils"
 import { AnimatedDiv } from "./ui/animated"
 import { Button } from "./ui/button"
 import { CancelSVG, PlaceHolderSVG } from "./ui/icons"
-import { useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { useFetchPlaces } from "@/lib/fetch-hooks"
 import { Skeleton } from "./ui/skeleton"
+import { AnimatePresence } from "framer-motion"
 
 
 const InfoSheet = () => {
     const searchParams = useSearchParams();
     const selectedPlace = searchParams.get('name');
+    const router = useRouter();
     const { places, isLoading } = useFetchPlaces();
 
     const place = places?.find(p => {
@@ -19,41 +21,71 @@ const InfoSheet = () => {
     })
 
     const handleCancelClick = () => {
-
+        const hash = window.location.hash;
+        const params = new URLSearchParams(searchParams);
+        params.delete('name');
+        router.replace(`?${params.toString()}${hash}`, { scroll: false })
     }
 
     const handleButtonClick = () => {
 
     }
 
-    return (selectedPlace &&
-        <div className='relative flex w-full place-self-center justify-center max-w-xl px-3 py-2'>
-            <AnimatedDiv className={cn(
-                'w-full md:w-auto md:min-w-96 relative flex flex-row',
-                'items-center justify-center p-4 rounded-lg',
-                'outline outline-2 outline-gray-500 pointer-events-auto'
-            )}>
-                <PlaceHolderSVG className="size-full max-h-20 max-w-20" />
+    const handleDivClick = () => {
+        place && updateHash({
+            zoom: 20,
+            lat: place.position[0],
+            lng: place.position[1]
+        }, true);
+    }
 
-                <div className="flex flex-col gap-y-1 mx-3 py-2 text-sm">
-                    {isLoading &&
-                        <Skeleton className='h-4 w-28 bg-gray-300 dark:bg-gray-500' />}
+    return (
+        <AnimatePresence>
+            {selectedPlace &&
+                <div className='relative flex w-full place-self-center justify-center max-w-xl px-3 py-2'>
+                    <AnimatedDiv
+                        key="info-sheet"
+                        className={cn(
+                            'w-full md:w-auto md:min-w-96 relative flex flex-row',
+                            'items-center justify-center p-4 rounded-lg',
+                            'outline outline-2 outline-gray-500 pointer-events-auto'
+                        )}
+                    >
+                        <PlaceHolderSVG
+                            onClick={handleDivClick}
+                            className={cn(
+                                'size-full max-h-20 max-w-20 rounded-lg bg-gray-500 border-gray-700 border-2',
+                                'border-transparent cursor-pointer hover:bg-gray-400 hover:border-gray-500',
+                            )}
+                        />
 
-                    <strong>{place?.name}</strong>
+                        <div className="flex flex-col gap-y-1 mx-3 py-2 text-sm">
+                            {isLoading &&
+                                <Skeleton className='h-4 w-28 bg-gray-300 dark:bg-gray-500' />}
 
-                    {place?.facility &&
-                        <span className='text-xs'>Floor {place.floor}, {place.facility}</span>}
+                            <strong
+                                onClick={handleDivClick}
+                                className='cursor-pointer hover:text-rose-200 transition-colors'
+                            >
+                                {place?.name}
+                            </strong>
 
-                    <Button onClick={handleButtonClick}>Get Directions</Button>
-                </div>
+                            {place?.facility &&
+                                <span className='text-xs'>
+                                    Floor {place.floor}, {place.facility}
+                                </span>}
 
-                <CancelSVG
-                    role='button'
-                    onClick={handleCancelClick}
-                    className="absolute top-2 right-2 fill-white"
-                />
-            </AnimatedDiv>
-        </div>
+                            <Button onClick={handleButtonClick}>Get Directions</Button>
+                        </div>
+
+                        <CancelSVG
+                            role='button'
+                            onClick={handleCancelClick}
+                            className="absolute top-2 right-2 fill-white"
+                        />
+                    </AnimatedDiv>
+                </div>}
+        </AnimatePresence>
     )
 }
 
