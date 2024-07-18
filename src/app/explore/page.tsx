c998c61aa0a3bb7c1d9315704c98d26081d20e18
Julius Pahama <pahamajulius@gmail.com>
2024-07-18 17:15:30 +0800

    wrap(InfoSheet): Wrap InfoSheet with suspense.

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,6 +1,7 @@
 import Search from "@/app/explore/search";
 import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const InfoSheet = dynamic(() => import('@/components/info-sheet'), { ssr: false });
 
@@ -11,7 +12,7 @@ type ExploreTabProps = {
 
 const ExploreTab = async () => {
     return (
-        <>
+        <Suspense>
             <section role='search' className={cn(
                 'relative w-full max-w-xl place-self-center',
                 'flex flex-col p-3 z-10 pointer-events-auto',
@@ -20,7 +21,7 @@ const ExploreTab = async () => {
             </section >
             <div className="flex flex-grow" />
             <InfoSheet />
-        </>
+        </Suspense>
     );
 };
 
